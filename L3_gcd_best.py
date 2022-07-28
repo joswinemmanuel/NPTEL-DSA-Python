44fea6a644a4d1fdfaf6bf839307c8d972f194c8
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-07-28 11:57:45 +0530

    Add files via upload

--- L3_gcd_best.py
+++ L3_gcd_best.py
@@ -1,11 +1,15 @@
 def gcd(m, n):
 
-    if m < n:
-        m, n = n, m
+    if n == 0:
+        return m
+    return gcd(n, m % n)
 
-    if m % n == 0:
-        return n
-    else:
-        return gcd(n, m % n)  # m % n < n always
+    # if m < n:
+    #     m, n = n, m
+
+    # if m % n == 0:
+    #     return n
+    # else:
+    #     return gcd(n, m % n)  # m % n < n always
 
 print(gcd(30, 75))
