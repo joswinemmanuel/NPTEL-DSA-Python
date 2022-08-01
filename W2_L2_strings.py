51f16ef6e086755d36d30f5cc75516931694b3e4
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-08-01 11:47:45 +0530

    Add files via upload

---
+++ W2_L2_strings.py
@@ -0,0 +1,18 @@
+city = 'Kerala\'s' # single quotes can be used
+title = "\"Joswin the great\"" # double quotes can be used
+paragraph = '''What's "going on"
+Why's "this" different''' # triple quotes can be used
+print(city)
+print(title)
+print(paragraph) 
+
+s1 = "Joswin"
+s2 = " Emmanuel"
+
+print(s1[1:5]) # string slicing
+
+print(s1[::-1]) # string reversing
+
+print(s1 + s2) # concatenation
+
+print(len(s1)) # getting length of the string
