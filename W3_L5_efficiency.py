6acf8295585e2fa3d0d7d0127f77a480261102f9
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-08-14 21:14:05 +0530

    Add files via upload

---
+++ W3_L5_efficiency.py
@@ -0,0 +1,14 @@
+""" Efficiency : Measure of time taken by an algorithm 
+as a function T(n) with respect to input size n """
+
+""" Big O notation
+T(n) = O(n)
+less time order:
+log(n) < n < n*log(n) < n**2 < n**3 < 2**n < n! """
+
+# Linear search efficiency is : O(n) for any lists
+# Binary search efficiency is : O(log(n)) for sorted lists
+
+" Python can do about 10**7 steps in 1 second"
+
+" use command : 'time python3 W3_L3_efficiency.py' to get time taken in LINUX"
