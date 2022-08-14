fa9a017090b6698c5d2ceb0119df9ce02cd46ccc
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-08-14 22:16:04 +0530

    Add files via upload

---
+++ W3_L6_selection_sort.py
@@ -0,0 +1,12 @@
+""" Selection sort """
+
+def selection_sort(lst):
+    n = len(lst)
+    for i in range(n):
+        min_index = i
+
+        for j in range(i+1, n):
+            if lst[j] < lst[min_index]:
+                min_index = j
+        lst[i], lst[min_index] = lst[min_index], lst[i]
+    return lst
