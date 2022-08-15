6bd85ce512d703bb559d55bb28c310285b5ed316
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-08-15 13:46:21 +0530

    Update W2_programming_assignment.py

--- W2_programming_assignment.py
+++ W2_programming_assignment.py
@@ -1,5 +1,15 @@
+""" 1) Write a function intreverse(n) that takes as input a positive integer n 
+and returns the integer obtained by reversing the digits in n.
+Here are some examples of how your function should work. """
+
 def intreverse(n):
     return int(str(n)[::-1])
+
+""" 2) Write a function matched(s) that takes as input a string s and checks if the brackets
+"(" and ")" in s are matched: that is, every "(" has a matching ")" after it and every ")" 
+has a matching "(" before it. Your function should ignore all other symbols that appear in s.
+Your function should return True if s has matched brackets and False if it does not.
+Here are some examples to show how your function should work """
 
 def matched(s):
     stack = []
@@ -16,6 +26,10 @@ def matched(s):
     else:
         return False
 
+""" 3) Write a function sumprimes(l) that takes as input a list of integers l
+and retuns the sum of all the prime numbers in l.
+Here are some examples to show how your function should work. """
+
 def sumprimes(l):
     def if_prime(n):
         if n <= 1:
@@ -24,10 +38,8 @@ def sumprimes(l):
             if n % i == 0:
                 return False
         return True
-
     sum = 0
     for i in l:
         if if_prime(i):
             sum += i
-    
     return sum
