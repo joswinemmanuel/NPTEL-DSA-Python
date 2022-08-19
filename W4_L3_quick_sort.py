9ab8009452d3ebfcb7eee7b990ca69553c280e24
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-08-19 13:25:12 +0530

    Update W4_L3_quick_sort.py

--- W4_L3_quick_sort.py
+++ W4_L3_quick_sort.py
@@ -45,4 +45,5 @@ print(quicksort(0, len(example)-1, example))
 # Time Complexity: Worst case time complexity is O(n**2) and average case time complexity is O(n*log(n))
 # Auxiliary Space: O(1)
 
+# Worst case is when the array is already sorted
 """ Generally built-in sort function in programming languages are quick sort """
